abde395483212f7159a48f897e407427898f1386
sudhanshu7037 <tomarsudhanshu7037@gmail.com>
2024-11-23 10:59:57 +0530

    first commit

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -18,7 +18,7 @@ const Login = ({handleLogin}) => {
 
   return (
     <div className='flex h-screen w-screen items-center justify-center'>
-        <div className='border-2 rounded-xl border-emerald-600 p-20'>
+        <div className='border-2 rounded-xl border-red-600 p-20'>
             <form 
             onSubmit={(e)=>{
                 submitHandler(e)
@@ -31,7 +31,7 @@ const Login = ({handleLogin}) => {
                     setEmail(e.target.value)
                 }}
                 required 
-                className='outline-none bg-transparent border-2 border-emerald-600 font-medium text-lg py-2 px-6 rounded-full placeholder:text-gray-400' type="email" placeholder='Enter your email' 
+                className='outline-none bg-transparent border-2 border-red-600 font-medium text-lg py-2 px-6 rounded-full placeholder:text-gray-400' type="email" placeholder='Enter your email' 
                 />
                 <input
                 value={password}
@@ -39,8 +39,8 @@ const Login = ({handleLogin}) => {
                     setPassword(e.target.value)
                 }}
                 required 
-                className='outline-none bg-transparent border-2 border-emerald-600 font-medium text-lg py-2 px-6 rounded-full mt-3 placeholder:text-gray-400' type="password" placeholder='Enter password' />
-                <button className='mt-7 text-white border-none outline-none hover:bg-emerald-700 font-semibold bg-emerald-600 text-lg py-2 px-8 w-full rounded-full placeholder:text-white'>Log in</button>
+                className='outline-none bg-transparent border-2 border-red-600 font-medium text-lg py-2 px-6 rounded-full mt-3 placeholder:text-gray-400' type="password" placeholder='Enter password' />
+                <button className='mt-7 text-white border-none outline-none hover:bg-red-700 font-semibold bg-red-600 text-lg py-2 px-8 w-full rounded-full placeholder:text-white'>Log in</button>
             </form>
         </div>
     </div>
--- src/components/other/AllTask.jsx
+++ src/components/other/AllTask.jsx
@@ -8,7 +8,7 @@ const AllTask = () => {
    
   return (
     <div className='bg-[#1c1c1c] p-5 rounded mt-5'>
-        <div className='bg-red-400 mb-2 py-2 px-4 flex justify-between rounded'>
+        <div className='bg-blue-400 mb-2 py-2 px-4 flex justify-between rounded'>
             <h2 className='text-lg font-medium w-1/5'>Employee Name</h2>
             <h3 className='text-lg font-medium w-1/5'>New Task</h3>
             <h5 className='text-lg font-medium w-1/5'>Active Task</h5>
